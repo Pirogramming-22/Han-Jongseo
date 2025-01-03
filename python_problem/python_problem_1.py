b6c8662199376484f1d2bf94091613296fa40943
HAN JONGSEO <pantene74@naver.com>
2025-01-03 17:33:38 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,6 @@
 num = 0
 input_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+if type(input_count) != int:
+  print("정수를 입력하세요")
+elif input_count != 1 or input_count != 2 or input_count != 3:
+  print("1,2,3 중 하나를 입력하세요")
