cff8f036e23a8f7eb8c438e7e11bf49c035f3fbf
HAN JONGSEO <pantene74@naver.com>
2025-01-03 17:55:08 +0900

    4단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,17 @@
 num = 0
-input_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-if type(input_count) != int:
-  print("정수를 입력하세요")
-elif input_count != 1 or input_count != 2 or input_count != 3:
-  print("1,2,3 중 하나를 입력하세요")
+
+while True: # 반복 조건 수정하기
+  try:
+    input_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+
+    if input_count not in [1, 2, 3]:
+      print("1,2,3 중 하나를 입력하세요")
+      continue
+
+    for i in range(input_count):
+      num += 1
+      print("playerA : ", num)
+      
+  except ValueError:
+    print("정수를 입력하세요")
+    continue
