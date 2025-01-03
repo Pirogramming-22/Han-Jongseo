ac76cbe2d1153310ad566f975e5540a42255eae9
HAN JONGSEO <pantene74@naver.com>
2025-01-03 18:15:12 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,37 @@
-num = 0
-player_flag = True
+def brGame():
+  num = 0
+  player_flag = True
 
-while True:
-  try:
-    input_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+  while True:
+    try:
+      input_count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
 
-    if input_count not in [1, 2, 3]:
-      print("1,2,3 중 하나를 입력하세요")
+      if input_count not in [1, 2, 3]:
+        print("1,2,3 중 하나를 입력하세요")
+        continue
+
+      if player_flag:
+        for i in range(input_count):
+          num += 1
+          if num == 31:
+            print("playerA win!")
+            return
+          print("playerA : ", num)
+        player_flag = not player_flag
+        continue
+      
+      if not player_flag:
+        for i in range(input_count):
+          num += 1
+          if num == 31:
+            print("playerB win!")
+            return
+          print("playerB : ", num)
+        player_flag = not player_flag
+        continue
+
+    except ValueError:
+      print("정수를 입력하세요")
       continue
 
-    if player_flag:
-      for i in range(input_count):
-        num += 1
-        if num == 31:
-          print("playerA win!")
-          break
-        print("playerA : ", num)
-      player_flag = not player_flag
-      continue
-    
-    if not player_flag:
-      for i in range(input_count):
-        num += 1
-        if num == 31:
-          print("playerB win!")
-          break
-        print("playerB : ", num)
-      player_flag = not player_flag
-      continue
-
-  except ValueError:
-    print("정수를 입력하세요")
-    continue
+brGame()
